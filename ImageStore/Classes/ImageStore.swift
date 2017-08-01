6f0dc56358adfaf249d1714b67966c295f2c7808
miup <contact@miup.blue>
2017-08-01 13:43:09 +0900

    implement ImageStroreConfig

--- ImageStore/Classes/ImageStore.swift
+++ ImageStore/Classes/ImageStore.swift
@@ -7,4 +7,26 @@
 
 import Foundation
 
-class ImageStore { }
+class ImageStore {
+    private(set) static var shared: ImageStore = ImageStore(ImageStoreConfig())
+
+    class func reset(config: ImageStoreConfig = ImageStoreConfig()) {
+        shared = ImageStore(config)
+    }
+
+    let config: ImageStoreConfig
+
+    private init(_ config: ImageStoreConfig) {
+        self.config = config
+    }
+}
+
+struct ImageStoreConfig {
+    let maxDownloadSize: UInt64
+    let cacheLimit: UInt64
+
+    init(maxDownloadSize: UInt64 = UInt64(2e8), cacheLimit: UInt64 = UInt64(200e8)) {
+        self.maxDownloadSize = maxDownloadSize
+        self.cacheLimit = cacheLimit
+    }
+}
